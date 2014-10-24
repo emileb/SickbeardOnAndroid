81bec82ba04114f9fa0d63c07ee6734ee83e4fa5
Emile Belanger <emile.belanger@gmail.com>
2014-10-24 14:56:28 +0100

    first commit

---
+++ gen/com/emtronics/sickbeardonandroid/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.emtronics.sickbeardonandroid;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/emtronics/sickbeardonandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package com.emtronics.sickbeardonandroid;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class MainActivity extends SherlockFragmentActivity {
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+
+	}
+}
